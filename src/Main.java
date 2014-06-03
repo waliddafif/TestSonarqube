2e39794f8ea11612c1056fea8b8feccd0a0a0024
wdafif <walid.dafif@gmail.com>
2014-06-03 20:11:54 +0200

    Modification Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,15 @@ public class Main {
 		System.out.println("Hello, Projet de test pour SonarQube !!!");
 
 	}
+	
+	private String field;
+
+	  public void use() {
+	    System.exit(33); // violation on findbugs rule: DM_EXIT
+	  }
+
+	  public void useFieldForLcom4() {
+	    System.out.println(field);
+	  }
 
 }
